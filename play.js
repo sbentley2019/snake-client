98dadc6e870bb4ab88277aa00542e88317febcad
sbentley2019 <sbentley000@gmail.com>
2019-10-23 16:29:50 +0000

    Created a play file that connects our snake to a specified host and port.

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '172.46.2.204',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
