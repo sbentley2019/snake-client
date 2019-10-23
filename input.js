c75bc7cd2077c6e1638c3fe18689e0733fd3519e
sbentley2019 <sbentley000@gmail.com>
2019-10-23 18:56:59 +0000

    Added functionality to input buttons w, a, s, d

--- input.js
+++ input.js
@@ -1,10 +1,27 @@
+let connection;
+
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
+
+  switch (key) {
+    case 'w':
+      connection.write("Move: up");
+      break;
+    case 'a':
+      connection.write("Move: left");
+      break;
+    case 's':
+      connection.write("Move: down");
+      break;
+    case 'd':
+      connection.write("Move: right");
+  }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
 
   stdin.setRawMode(true);
